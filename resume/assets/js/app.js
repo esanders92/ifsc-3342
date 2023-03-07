0bd7b226b00f83af4ca035f1534b8479a8afe3b1
esanders92 <edsanders@ualr.edu>
2023-03-07 14:56:59 +0000

    [2023/03/07] - Added content and styling to Resume project

--- resume/assets/js/app.js
+++ resume/assets/js/app.js
@@ -1,7 +1,14 @@
-$(document).ready(function () {
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
     });
-
-});
+}
